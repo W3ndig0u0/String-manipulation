bd340e9d59b1415148a088d887ce7e90f4ce0b57
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-02-25 11:13:00 +0100

    Sting manip

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String_manipulation
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] highScores = { "LENA 100000", "MICKE 23004", "MARTIN 2000" };
+
+      //   string[] names = highScores.Split(' ');
+      string space = string.Join("\n", highScores);
+      Console.WriteLine(space);
+    }
+  }
+}
